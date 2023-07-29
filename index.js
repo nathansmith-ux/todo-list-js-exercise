6a2be166d2aedd38498692bdffb53225c1940a7f
Nathan Smith <nathanrmsmith@gmail.com>
2023-07-29 14:58:44 -0700

    OO exercise

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
+/* ORIGINAL - LOTS OF FUNCTIONS 😨😨
+
 // Arrays to keep track of each task's state
+
 const taskTitles = [];
 const taskComplete = [];
+const taskDescriptions = [];
 
 // Create a new task by adding to the arrays
 // A new task will be created as incomplete
-function newTask(title) {
+function newTask(title, description) {
   taskTitles.push(title);
+  taskDescriptions.push(description)
   taskComplete.push(false);
 }
 
@@ -29,3 +34,34 @@ newTask("Do Laundry"); // task 1
 logTaskState(0); // Clean Cat Litter has not been completed
 completeTask(0);
 logTaskState(0); // Clean Cat Litter has been completed
+*/
+
+
+// USING OO 😊😊
+function newTask(title, description) {
+  const task = {
+    title: title,
+    description: description,
+    complete: false,
+
+    logState: function() {
+      console.log(`${this.title} has${this.complete ? " " : " not "}been completed`)
+    },
+
+    markCompleted: function () {
+      this.complete = true;
+    }
+
+  };
+  return task;
+}
+
+// DRIVER CODE BELOW
+
+const task1 = newTask("Clean Cat Litter", "Take all the 💩 out of the litter box");
+const task2 = newTask("Do Laundry", "😨");
+const tasks = [task1, task2];
+
+task1.logState();
+task1.markCompleted();
+task1.logState();
